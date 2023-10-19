4021945da3bec608390f969892f7ea61beadbcf8
abhay <abhay290@gmail.com>
2023-10-19 16:31:20 +0530

    fix bug

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
-const mongoURI=   "mongodb://localhost:27017/SelectNotes?directConnection=true"
+const mongoURI = "mongodb://localhost:27017/SelectNotes?directConnection=true"
 
 //for conecting to database
-const connnectToMongo = async() => {
-   await mongoose.connect(mongoURI);
-    console.log("Hey I'm Connected");
+const connnectToMongo = async () => {
+    await mongoose.connect(mongoURI);
+    console.log("Hey I'm Connected to MongoDB");
 }
 
 
-module.exports= connnectToMongo
+module.exports = connnectToMongo
