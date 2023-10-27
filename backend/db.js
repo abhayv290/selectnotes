b4725749aeeadc5732e166646928be26e6256e7f
abhay <abhay290@gmail.com>
2023-10-27 15:56:26 +0530

    fix error while connecting web server

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
-<<<<<<< HEAD
 
 const mongoURI = process.env.MONGO_URI
 
 
-=======
-
-
-const mongoURI = process.env.MONGO_URI
->>>>>>> 514bcf70408eb8b31cb7b7614eae35f8738e558f
 
 //for conecting to database
 const connnectToMongo = async () => {
